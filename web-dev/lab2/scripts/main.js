2d4f8cb9f3d6094e7b52856aa5f5d574942b2a4f
ajaymatters <ajaywpps@gmail.com>
2018-10-05 00:36:16 +0530

    Add files via upload

--- web-dev/lab2/scripts/main.js
+++ web-dev/lab2/scripts/main.js
@@ -1,1 +1,55 @@
 
+
+function dispGrade_sub1() {	
+
+		var x = parseInt(document.getElementById("sub1").value);
+		
+		if (x >= 90) {
+			document.getElementById("sub1_grade").innerHTML = "S"
+		}
+
+		else if (x >= 80 && x <90) {
+			document.getElementById("sub1_grade").innerHTML = "A"	
+		}
+
+		else if (x >= 70 && x < 80) {
+			document.getElementById("sub1_grade").innerHTML = "C"	
+		}
+
+		else {
+			document.getElementById("sub1_grade").innerHTML = "Fail" + x
+		}
+
+}
+
+function dispGrade_sub2() {
+	var sub2 = (document.getElementById('sub2'));
+
+	if (sub2 > 100) {
+		document.getElementById('sub2_grade').innerHTML = "Please enter a value below 100"
+	}
+
+	else if (sub2 < 0) {
+		document.getElementById('sub2_grade').innerHTML = "Please enter a value above 0"		
+	}
+
+	else {
+
+		if (sub2 >= 90) {
+			document.getElementById('sub2_grade').innerHTML = "S"
+		}
+
+		else if (sub2 >= 80 && sub2 <90) {
+			document.getElementById('sub2_grade').innerHTML = "A"	
+		}
+
+		else if (sub2 >= 70 && sub2 < 80) {
+			document.getElementById('sub2_grade').innerHTML = "C"	
+		}
+
+		else {
+			document.getElementById('sub2_grade').innerHTML = "Fail"
+		}
+
+	}
+}
