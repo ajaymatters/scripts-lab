8b285e4cadae055087696ab9585dc9aee22994f5
ajaymatters <ajaywpps@gmail.com>
2018-10-05 00:51:58 +0530

    Add files via upload

--- web-dev/lab2/scripts/main.js
+++ web-dev/lab2/scripts/main.js
@@ -4,51 +4,62 @@ function dispGrade_sub1() {
 
 		var x = parseInt(document.getElementById("sub1").value);
 
-		if (x >= 90) {
-			document.getElementById("sub1_grade").innerHTML = "S"
+		if (x > 100) {
+			document.getElementById("sub2_grade").innerHTML = "Please enter a value below 100"
 		}
 
-		else if (x >= 80 && x <90) {
-			document.getElementById("sub1_grade").innerHTML = "A"	
-		}
-
-		else if (x >= 70 && x < 80) {
-			document.getElementById("sub1_grade").innerHTML = "C"	
+		else if (x < 0) {
+			document.getElementById("sub2_grade").innerHTML = "Please enter a value above 0"		
 		}
 
 		else {
-			document.getElementById("sub1_grade").innerHTML = "Fail"
-		}
+			if (x >= 90) {
+				document.getElementById("sub1_grade").innerHTML = "S"
+			}
+
+			else if (x >= 80 && x <90) {
+				document.getElementById("sub1_grade").innerHTML = "A"	
+			}
+
+			else if (x >= 70 && x < 80) {
+				document.getElementById("sub1_grade").innerHTML = "C"	
+			}
+
+			else {
+				document.getElementById("sub1_grade").innerHTML = "Fail"
+			}
+	}
 
 }
 
 function dispGrade_sub2() {
-	var sub2 = (document.getElementById('sub2'));
 
-	if (sub2 > 100) {
-		document.getElementById('sub2_grade').innerHTML = "Please enter a value below 100"
+	var y = parseInt(document.getElementById("sub2").value);
+
+	if (y > 100) {
+		document.getElementById("sub2_grade").innerHTML = "Please enter a value below 100"
 	}
 
-	else if (sub2 < 0) {
-		document.getElementById('sub2_grade').innerHTML = "Please enter a value above 0"		
+	else if (y < 0) {
+		document.getElementById("sub2_grade").innerHTML = "Please enter a value above 0"		
 	}
 
 	else {
 
-		if (sub2 >= 90) {
-			document.getElementById('sub2_grade').innerHTML = "S"
+		if (y >= 90) {
+			document.getElementById("sub2_grade").innerHTML = "S"
 		}
 
-		else if (sub2 >= 80 && sub2 <90) {
-			document.getElementById('sub2_grade').innerHTML = "A"	
+		else if (y >= 80 && y <90) {
+			document.getElementById("sub2_grade").innerHTML = "A"	
 		}
 
-		else if (sub2 >= 70 && sub2 < 80) {
-			document.getElementById('sub2_grade').innerHTML = "C"	
+		else if (y >= 70 && y < 80) {
+			document.getElementById("sub2_grade").innerHTML = "C"	
 		}
 
 		else {
-			document.getElementById('sub2_grade').innerHTML = "Fail"
+			document.getElementById("sub2_grade").innerHTML = "Fail"
 		}
 
 	}
