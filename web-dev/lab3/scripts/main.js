e6d279c5507babd2ff84bec639ce70f4b05173e9
ajaymatters <ajaywpps@gmail.com>
2018-10-05 02:34:09 +0530

    Add files via upload

--- web-dev/lab3/scripts/main.js
+++ web-dev/lab3/scripts/main.js
@@ -1,1 +1,8 @@
-
+function yesnoCheck() {
+    if (document.getElementById('Pizza').checked || document.getElementById('Burger').checked) {
+        document.getElementById('ifPizza').style.visibility = 'visible';
+    }
+    else {
+    	document.getElementById('ifPizza').style.visibility = 'hidden';
+    }
+}
